c6d3ae66a8826f5a9d84f64a37f0ee7b2eec0ae5
Antonio Zdelican <antonio.zdelican@gmail.com>
2021-12-07 10:56:45 +0100

    Added api request tests

--- FlickrWalkerTests/API/FlickrAPIRequestTests.swift
+++ FlickrWalkerTests/API/FlickrAPIRequestTests.swift
@@ -6,29 +6,49 @@
 //
 
 import XCTest
+@testable import FlickrWalker
 
 class FlickrAPIRequestTests: XCTestCase {
     
-    // TODO: simple tests for FlickrAPIRequest if all the variables are correct
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_initSearch_withSearchRequestData_shouldReturnFlickrAPISearchRequest() {
+        let lat = 45.81958312506764
+        let lon = 16.01557795876301
+        let searchRequest = SearchRequest(lat: lat, lon: lon)
+        
+        let flickrAPISearchRequest = FlickrAPIRequest.search(searchRequest)
+        
+        XCTAssertEqual(flickrAPISearchRequest.baseUrl, "https://api.flickr.com/services/rest/")
+        XCTAssertEqual(flickrAPISearchRequest.path, "")
+        XCTAssertEqual(flickrAPISearchRequest.method, .get)
+        XCTAssertNil(flickrAPISearchRequest.headers)
+        XCTAssertEqual(flickrAPISearchRequest.parameters?["lat"] as? Double, lat)
+        XCTAssertEqual(flickrAPISearchRequest.parameters?["lon"] as? Double, lon)
+        XCTAssertEqual(flickrAPISearchRequest.parameters?["api_key"] as? String, "2c82bf1222ff33464507511cc73b52c2")
+        XCTAssertEqual(flickrAPISearchRequest.parameters?["method"] as? String, "flickr.photos.search")
+        XCTAssertEqual(flickrAPISearchRequest.parameters?["format"] as? String, "json")
+        XCTAssertEqual(flickrAPISearchRequest.parameters?["nojsoncallback"] as? Int, 1)
+        XCTAssertEqual(flickrAPISearchRequest.parameters?["min_upload_date"] as? String, "2000-01-01T00:00:00Z")
+        XCTAssertEqual(flickrAPISearchRequest.parameters?["geo_context"] as? Int, 2)
+        XCTAssertEqual(flickrAPISearchRequest.parameters?["radius"] as? Double, 0.1)
+        XCTAssertEqual(flickrAPISearchRequest.requestType, .data)
+        XCTAssertEqual(flickrAPISearchRequest.responseType, .json)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_initImage_withImageRequestData_shouldReturnFlickrAPIImageRequest() {
+        let id = "49159180388"
+        let secret = "f73c35240d"
+        let server = "65535"
+        let imageRequest = ImageRequest(id: id, server: server, secret: secret)
+        
+        let flickrAPIImageRequest = FlickrAPIRequest.image(imageRequest)
+        
+        XCTAssertEqual(flickrAPIImageRequest.baseUrl, "https://live.staticflickr.com/")
+        XCTAssertEqual(flickrAPIImageRequest.path, "/\(server)/\(id)_\(secret)_w.jpg")
+        XCTAssertEqual(flickrAPIImageRequest.method, .get)
+        XCTAssertNil(flickrAPIImageRequest.headers)
+        XCTAssertNil(flickrAPIImageRequest.parameters)
+        XCTAssertEqual(flickrAPIImageRequest.requestType, .download)
+        XCTAssertEqual(flickrAPIImageRequest.responseType, .file)
     }
 
 }
